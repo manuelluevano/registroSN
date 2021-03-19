36855c2ffa15d8c0395812280edafafcdd766cb9
Manuel Luevano <manuelluevano@hotmail.com>
2021-03-19 13:28:36 -0600

    DISEÑO: diseño a registroSN.php

--- registroSN.php
+++ registroSN.php
@@ -2,194 +2,199 @@
    include_once 'includes/funciones/funciones.php'; 
    include_once 'includes/templates/header.php';
 ?>
-<div class="navegacion">
-        <a href="index.php">Regresar</a>
+    <div class="navegacion">
     </div>
 
-  <h2>Registro de datos</h2>
-<div class="formularioRegistro">
-    <form action="#" id="registroSN">
-    <div class="notificacion">
-    </div>
- 
-    <div class="campo">
-        <label for="mayorista">Mayorista:</label>
-        <select name="mayorista" id="inpMayorista">
-        
-        <option value="publico">Publico</option> 
-
-
-        <!-- OBTENER LOS MAYORISTAS DE LA BASE DE DATOS  -->
-
-        <?php 
-        
-        $mayoristas = obtenerMayoristas(); 
-         
-        // var_dump($mayoristas); 
-
-        // SI EXISTEN VALORES QUE SE EJECUTE, SI NO, NO REALICE NADA.
+<div class="barraEnlaces">
+    <h2>Registro de datos</h2>
+    <div class="formularioRegistro">
+        <form action="#" id="registroSN">
+        <div class="notificacion">
+        </div>
     
-        while($mayorista = mysqli_fetch_assoc($mayoristas)){?>
+    
+    <div class="campos">
+
+        <div class="campo">
+            <label for="mayorista">Mayorista:</label>
+            <select name="mayorista" id="inpMayorista">
             
-            <option  value="<?php echo $mayorista['nombre']; ?>"><?php echo $mayorista['nombre']; ?></option> 
-
-        <?php } ?>
+            <option value="publico">Publico</option> 
 
 
-    </select>
+            <!-- OBTENER LOS MAYORISTAS DE LA BASE DE DATOS  -->
+
+            <?php 
+            
+            $mayoristas = obtenerMayoristas(); 
+            
+            // var_dump($mayoristas); 
+
+            // SI EXISTEN VALORES QUE SE EJECUTE, SI NO, NO REALICE NADA.
+        
+            while($mayorista = mysqli_fetch_assoc($mayoristas)){?>
+                
+                <option  value="<?php echo $mayorista['nombre']; ?>"><?php echo $mayorista['nombre']; ?></option> 
+
+            <?php } ?>
+
+
+            </select>
         </div>
 
         <div class="campo">
-            <label for="">Modelo Iphone:</label>
-            <select name="modelo" id="modelo">
-                <option >Seleccionar</option>
-                
-                <!-- Obtener los modelos desde la base de datos -->
-                <?php 
-        
-                $modelos = obtenerModelosIphone(); 
-                
-                // var_dump($mayoristas); 
-
-                // SI EXISTEN VALORES QUE SE EJECUTE, SI NO, NO REALICE NADA.
-            
-                while($modelo = mysqli_fetch_assoc($modelos)){?>
+                <label for="">Modelo Iphone:</label>
+                <select name="modelo" id="modelo">
+                    <option >Seleccionar</option>
                     
-                    <option  value="<?php echo $modelo['modelo']; ?>"><?php echo $modelo['modelo']; ?></option> 
-
-                <?php } ?>  <!-- CIERRE CONDICIONAL IF -->
-                        
-            </select>
-        </div>  
-
-
-        <div class="campo">
-            <label for="">Número de Serie (SN):</label>
-            <input type="text" id="sn" maxlength="13">
-        </div>  
-
-        <div class="campo">
-            <label for="">Método::</label>
-            <select name="metodo" id="metodo">
-                <option>Seleccionar</option>
-                
-                <!-- Obtener los modelos desde la base de datos -->
-                <?php 
-        
-                $metodos = obtenerMetodos(); 
-                
-                // var_dump($mayoristas); 
-
-                // SI EXISTEN VALORES QUE SE EJECUTE, SI NO, NO REALICE NADA.
+                    <!-- Obtener los modelos desde la base de datos -->
+                    <?php 
             
-                while($metodo = mysqli_fetch_assoc($metodos)){?>
+                    $modelos = obtenerModelosIphone(); 
                     
-                    <option  value="<?php echo $metodo['metodo']; ?>"><?php echo $metodo['metodo']; ?></option> 
+                    // var_dump($mayoristas); 
 
-                <?php } ?>  <!-- CIERRE CONDICIONAL IF -->
+                    // SI EXISTEN VALORES QUE SE EJECUTE, SI NO, NO REALICE NADA.
+                
+                    while($modelo = mysqli_fetch_assoc($modelos)){?>
                         
-            </select>
+                        <option  value="<?php echo $modelo['modelo']; ?>"><?php echo $modelo['modelo']; ?></option> 
+
+                    <?php } ?>  <!-- CIERRE CONDICIONAL IF -->
+                            
+                </select>
+        </div>  
+
+
+        <div class="campo">
+                <label for="">Número de Serie (SN):</label>
+                <input type="text" id="sn" maxlength="13">
         </div>  
 
         <div class="campo">
-            <label for="">Fecha:</label>
-            <input type="date" id="fecha">
+                <label for="">Método::</label>
+                <select name="metodo" id="metodo">
+                    <option>Seleccionar</option>
+                    
+                    <!-- Obtener los modelos desde la base de datos -->
+                    <?php 
+            
+                    $metodos = obtenerMetodos(); 
+                    
+                    // var_dump($mayoristas); 
+
+                    // SI EXISTEN VALORES QUE SE EJECUTE, SI NO, NO REALICE NADA.
+                
+                    while($metodo = mysqli_fetch_assoc($metodos)){?>
+                        
+                        <option  value="<?php echo $metodo['metodo']; ?>"><?php echo $metodo['metodo']; ?></option> 
+
+                    <?php } ?>  <!-- CIERRE CONDICIONAL IF -->
+                            
+                </select>
+        </div>  
+
+        <div class="campo">
+                <label for="">Fecha:</label>
+                <input type="date" id="fecha">
         </div> 
-        
+            
         <div class="campo">
-            <label for="">Costo:</label>
-            <input type="number" id="costo">
+                <label for="">Costo:</label>
+                <input type="number" id="costo">
         </div>
 
         <div class="campo">
-            <label for="">Observaciones:</label>
-            <input type="text" placeholder="Escribe aqui tus comentarios" id="observaciones">
+                <label for="">Observaciones:</label>
+                <input type="text" placeholder="Escribe aqui tus comentarios" id="observaciones">
+        </div>
+    </div>
+
+        <div class="campo-enviar">
+                <input type="submit" value="Guardar" id="accion" class="btn btn-success">
         </div>
 
-        <div class="campo enviar">
-            <input type="submit" value="Guardar" id="accion">
-        </div>
+        </form>
 
-    </form>
+    </div>
+
+
+    <section class="Listado-registrados">
+        <h2>Informacion de sn</h2>
+
+
+
+        <!-- Imprimir todos los elemtos de la base de datos  -->
+        <div class="contenedor-tabla">
+            <table id="listado_SN" class="table">
+                <thead>
+                <tr>
+                    <th>Mayorista: </th>
+                    <th>Modelo Iphone: </th>
+                    <th>SN: </th>
+                    <th>Método: </th>
+                    <th>Fecha: </th>
+                    <th>Costo: </th>
+                    <th>Observaciones: </th>
+                    <th>Acciones</th>
+
+                </tr>
+                </thead>
+
+
+                <tbody>
+
+                        <!-- Obtener los modelos desde la base de datos -->
+                    <?php $registros = obtenerRegistrosSN(); 
+                    
+                    
+                    //Validar que existan datos
+                    if($registros->num_rows){
+
+                            // SI EXISTEN VALORES QUE SE EJECUTE, SI NO, NO REALICE NADA.
+                        
+                            while($registro = mysqli_fetch_assoc($registros)){
+                            
+                            //var_dump($registro); ?>
+
+                                        
+                                    <tr class="tabla-registro-sn">
+
+                                        <td><?php echo $registro['nombre_mayorista']?></td>
+                                        <td><?php echo $registro['modelo_iphone']?></td>
+                                        <td><?php echo $registro['numero_serie']?></td>
+                                        <td><?php echo $registro['metodo_aplicado']?></td>
+                                        <td><?php echo $registro['fecha_registro']?></td>
+                                        <td><?php echo $registro['costo_registro']?></td>
+                                        <td><?php echo $registro['observaciones_registro']?></td>
+                                <div class="campo-enviar">
+   
+                                        <td class="acciones">
+                                            <a href="editar_sn?id=<?php echo $registro['id_registro_datos']?>" class="btn btn-editar">
+                                                <i class="fas fa-pen-square"></i>
+                                            </a>
+
+                                            <button data-id="<?php echo $registro['id_registro_datos']?>" class="btn-borrar btn" type="button">
+                                                <i class="fas fa-trash"></i>
+                                            </button>
+                                        </td>
+
+                                    </tr>
+                                    <?php    }
+                    } ?>  <!-- CIERRE CONDICIONAL IF -->
+                    
+                    
+                        
+                        
+
+                <tbody>
+            </table>
+        </div>
+    
+
+    </section>
 
 </div>
-
-
-<section class="Listado-registrados">
-    <h2>Informacion de sn</h2>
-
-
-
-    <!-- Imprimir todos los elemtos de la base de datos  -->
-    <div class="contenedor-tabla">
-          <table id="listado_SN">
-            <thead>
-              <tr>
-                <th>Mayorista: </th>
-                <th>Modelo Iphone: </th>
-                <th>SN: </th>
-                <th>Método: </th>
-                <th>Fecha: </th>
-                <th>Costo: </th>
-                <th>Observaciones: </th>
-                <th>Acciones</th>
-
-              </tr>
-            </thead>
-
-
-            <tbody>
-
-                    <!-- Obtener los modelos desde la base de datos -->
-                <?php $registros = obtenerRegistrosSN(); 
-                
-                
-                //Validar que existan datos
-                if($registros->num_rows){
-
-                        // SI EXISTEN VALORES QUE SE EJECUTE, SI NO, NO REALICE NADA.
-                    
-                        while($registro = mysqli_fetch_assoc($registros)){
-                        
-                        //var_dump($registro); ?>
-
-                                    
-                                <tr class="tabla-registro-sn">
-
-                                    <td><?php echo $registro['nombre_mayorista']?></td>
-                                    <td><?php echo $registro['modelo_iphone']?></td>
-                                    <td><?php echo $registro['numero_serie']?></td>
-                                    <td><?php echo $registro['metodo_aplicado']?></td>
-                                    <td><?php echo $registro['fecha_registro']?></td>
-                                    <td><?php echo $registro['costo_registro']?></td>
-                                    <td><?php echo $registro['observaciones_registro']?></td>
-                                    
-                                    <td class="acciones">
-                                        <a href="editar_sn?id=<?php echo $registro['id_registro_datos']?>" class="btn btn-editar">
-                                            <i class="fas fa-pen-square"></i>
-                                        </a>
-
-                                        <button data-id="<?php echo $registro['id_registro_datos']?>" class="btn-borrar btn" type="button">
-                                            <i class="fas fa-trash"></i>
-                                        </button>
-                                    </td>
-
-                                </tr>
-                                <?php    }
-                 } ?>  <!-- CIERRE CONDICIONAL IF -->
-                
-                
-                    
-                    
-
-            <tbody>
-        </table>
-    </div>
-   
-
-</section>
-
-
 
 
 <script src="js/vendor/modernizr-3.11.2.min.js"></script>
